56262241a9e5b24de6918755210626bb34a1d442
Christian77s <christian.chinrichman@gmail.com>
2023-10-17 10:53:01 +0700

    my very first commit

--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -1,10 +1,10 @@
-    @extends('_layout.app')
+    @extends('_layouts.app')
     @section('content')
     <section class="checkout">
         <div class="container">
             <div class="row text-center">
                 <div class="col-lg-12 col-12">
-                    <img src="{{assets('images/ill_register.png')}}" height="400" class="mb-5" alt=" ">
+                    <img src="{{asset('images/ill_register.png')}}" height="400" class="mb-5" alt=" ">
                 </div>
                 <div class=" col-lg-12 col-12 header-wrap mt-4">
                     <p class="story">
